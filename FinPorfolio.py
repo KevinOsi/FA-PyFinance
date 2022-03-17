4ef4cd85a979b22399c97952f0fbb9336c1444ed
Kevin Osicki <KevinOsi@users.noreply.github.com>
2022-03-16 20:56:08 -0600

    updates to Portfolio stuff, plots etc..

--- FinPorfolio.py
+++ FinPorfolio.py
@@ -12,6 +12,7 @@ import pandas_ta as ta
 import yfinance as yf
 import numpy as np
 
+
 import os
 from os import listdir
 from os.path import isfile, join
@@ -22,43 +23,128 @@ from plotly.subplots import make_subplots
 
 class FinPortfolio():
 
-    def __init__(self, symbols, **kwargs):
-        self.symbols = symbols
+    def __init__(self, **kwargs):
+        
+        self.symbols = kwargs.get("symbols", '')
         self.df = pd.DataFrame()
         self.period = "5y"
 
-        self.position = ''  #positions taken in each company for analysis
+        #start and end dates for plotting
+        self.start = kwargs.get("start", '')
+        self.end = kwargs.get("end", '')
+
+        #positions taken in each company for analysis
+        self.weights = kwargs.get("weights", [])
 
 
 
-    def getPortfolio(self):
+    def get_portfolio(self, **kwargs):
         """
         Get portfolio from given symbols group, downloads data from Yahoo finance
 
         Calculates cummulative daily returns for analysis per each stock and saves in dataframe
 
         """
-        print('Getting data from yfinance')
-        data = pd.DataFrame
-        data = yf.download(self.symbols, group_by='ticker', period=self.period)
 
-        for symbol in self.symbols:
-            #self.df[symbol] = data[symbol]['Adj Close']
-            #self.df[symbol + "_ret"] = (data[symbol]['Adj Close'] / data[symbol]['Adj Close'].shift(1)) - 1
+        """
+        Retrieves data from file or yfinance call based on supplied criteria
+        
+        keys:
+            default = Boolean, use default settings or not
+           
+            file = string, full file name to read
+
+        Args:
+            () returns default values from yfinance, 5Y, 1D
+
+            (default=False) pulls from supplied time frame or interval
+
+            (file='') reads csv data from file name directly
             
-            #return cummulative return for each symbol
-            self.df[symbol] = (data[symbol]['Adj Close'] / data[symbol]['Adj Close'].shift(1)) - 1
+
+        """ 
+        
+        
+        #check if file location given to CSV file, else load from ta's ticker function from yfinance
+        if 'file' in kwargs:
+            print('Getting data from CSV')
+            # Try to get the file and if it doesn't exist issue a warning
+            try:
+                self.df = pd.read_csv(kwargs['file'], index_col=0, parse_dates=True)
+
+                #save symbols to self.symbols
+                self.symbols = list(self.df.filter(regex='daily_').columns.str.replace("daily_",""))
+               
+
+            except FileNotFoundError:
+                print("File Doesn't Exist")
+            else:
+                return self.df
+       
+        #default is to read directly from yahoo finance via pandas_ta
+        else:
+            print('Getting data from yfinance')
+            
+            #check for symbols are given
+            if len(self.symbols) == 0:
+                print('No symbols given, check portfolio creation or self.symbols')
+            
+            else:
+                    
+                #default values check, will use 5y / 1d values, ELSE use given date time frame
+                if kwargs.get('default', True) is True:
+                    print('Using default values, 5Y and 1d')
+                    
+                    data = yf.download(self.symbols, group_by='ticker', period=self.period)
+                    
+                else:
+                    data = yf.download(self.symbols, group_by='ticker', start=self.start, end=self.end)
+                
+
+                for symbol in self.symbols:
+                
+                    #return cummulative return for each symbol
+                    self.df["price_" + symbol] = data[symbol]['Adj Close']
+                    self.df["daily_" + symbol] = (data[symbol]['Adj Close'] / data[symbol]['Adj Close'].shift(1)) - 1
 
 
-    def PlotCorrelation(self):
+
+    def save_portfolio (self, folder, **kwargs):
+        """
+        Saves the porfolio data to a CSV files at a folder location 
+        
+        keys:
+            all=True  save all data or just the     
+
+        Args:
+            (foldername) - saves all data to <Folder>/symbol.csv
+
+            (foldername, all=False) - saves only core data to <Folder>/symbol.csv
+         
+        
+        """ 
+        
+        
+        try:
+            file = folder + "Portfolio" + '.csv'
+            self.df.to_csv(file)
+        except Exception as ex:
+            print("Couldn't save file")
+
+
+
+    def plot_correlation(self):
         """
         Calculates and plots the correlation matrix 
 
         """
-
-        #calulate correlation matrix
-        correlation_matrix = self.df.corr(method='pearson')
         
+        #calulate correlation matrix, scrub daily prefix
+        correlation_matrix = self.df.filter(regex='daily_').corr(method='pearson')
+        correlation_matrix.columns = correlation_matrix.columns.str.replace("daily_","")
+        correlation_matrix.index = correlation_matrix.index.str.replace("daily_","")
+
+
         #new graph object
         fig = go.Figure()
 
@@ -78,13 +164,92 @@ class FinPortfolio():
         #plot object
         fig.show()
 
+    def plot_riskbox(self):
+        """
+        Calculates and plots the box risk approximation via box plots for all stocks' cummulative return
+
+        
+        """
+                
+        #calulate correlation matrix, scrub daily prefix
+        riskboxdf = pd.DataFrame()
+        riskboxdf = self.df.filter(regex='daily_')
+        riskboxdf.columns = riskboxdf.columns.str.replace("daily_","")
+        
+        
+        #new graph object
+        fig = go.Figure() 
+        
+        #generate box plots
+        for column in riskboxdf:
+            boxret = go.Box(y=riskboxdf[column], name=column) 
+            fig.add_trace(boxret)
+
+        #add plot information
+        fig.update_layout(height=1000, width=1400, showlegend=True, title=f"risk box plot for stocks over {self.period}")
 
 
-#savePortfolio
+        fig.show()
+
+
+    def plot_cummulative(self):
+        """
+        Calculates and plots the cummulative gains for all stocks
+
+        """
+
+        cummulativedf = (1 + self.df.filter(regex='daily_')).cumprod()
+        cummulativedf.columns = cummulativedf.columns.str.replace("daily_","")
+
+        #new graph object
+        fig = go.Figure() 
+        
+        #generate box plots
+        for column in cummulativedf:
+            cummret = go.Scatter(x=cummulativedf.index, y=cummulativedf[column], name=column) 
+            fig.add_trace(cummret)
+
+        #add plot information
+        fig.update_layout(height=1000, width=1400, showlegend=True, title=f"cummulative returns for stocks over {self.period}")
+
+
+        fig.show()
+        
+
+    def PortfolioPerformance(self, **kwargs):
+        """
+        WORKING ON THIS SECTION! 
+
+        Calculates and plots the cummulative gains for all stocks
+       
+
+        """
+       
+
+        returns = self.df.filter(regex='price_').pct_change()
+        returns.columns = returns.columns.str.replace('price_', '')
+        meanreturns = returns.mean()
+        covMatrix = returns.cov()
+
+        #get weights (just making an even distro for now)
+        self.weights = np.ones(len(returns.columns)) * 1/len(returns.columns)
+        print(self.weights)
+
+
+        #applies returns vs weights over trading period
+        weightedreturns = np.sum(meanreturns*self.weights)*252
+        
+        #calc porfolio variance
+        std = np.sqrt( np.dot(self.weights.T, np.dot(covMatrix, self.weights))) * np.sqrt(252)
+
+        print(std)
+
+
+
 
 #performance view
 
-#correlation, some sort of optimizer?
+#some sort of optimizer?
 
 #risk returns
 
@@ -92,12 +257,16 @@ class FinPortfolio():
 
 if __name__ == '__main__':
 
-    mySymbols = ["TSLA", "AAPL", "FB", "AMD", "MSFT", "COST", "AMZN", "GOOG"]
-    myPortfolio = FinPortfolio(mySymbols)
+    #mySymbols = ["TSLA", "AAPL", "FB", "AMD", "MSFT", "COST", "AMZN", "GOOG"]
+    #myPortfolio = FinPortfolio(symbols=mySymbols, start='2020-01-01', end='2022-01-01')
+    myPortfolio = FinPortfolio()
 
-    myPortfolio.getPortfolio()
-    myPortfolio.PlotCorrelation()
-    
+    #myPortfolio.get_portfolio(default=True)  
+    #myPortfolio.save_portfolio('D:/Temp/StockData/')    
 
+    myPortfolio.get_portfolio(file="D:/Temp/StockData/Portfolio.csv")
+    #myPortfolio.plot_correlation()
+    myPortfolio.plot_riskbox()
+    #myPortfolio.plot_cummulative()
 
-
+    #myPortfolio.PortfolioPerformance()
--- README.md
+++ README.md
@@ -5,25 +5,25 @@ My basic plotting and analysis library for fiance
 
 ## Features:
 
-* data pull from CSV or yahoo finance
-* save to CSV
-* Plotting for RSI, Ichimoku, SMA, MACD
-* Multi-plot for multiple technical indicators
+    * data pull from CSV or yahoo finance
+    * save to CSV
+    * Plotting for RSI, Ichimoku, SMA, MACD
+    * Multi-plot for multiple technical indicators
 
 
 ## Requirements:
 
     * Pandas_ta https://github.com/twopirllc/pandas-ta
-    * 
+    * pyPortfolioOpt https://github.com/robertmartin8/PyPortfolioOpt  (future)
 
 
 	
 ## TODO:
-* create an accompanying jupyter notebook to help with usage
-* Portfolio analysis functionality
-* additional plots that seem to help
-* Add in more trade signals to relevant indicators to assist with decision making and interpretation
-* Method to pass along custom settings to each plot routine
-* Method to use each plot routine solo with no multiplot call
-
-* investigate FFT analysis to find fundamentals for SMA or other charting
+    * create an accompanying jupyter notebook to help with usage
+    * Portfolio analysis functionality 
+        - reading up on PyPortfolioOpt
+        - manual caluclations for portfolio optimizations using efficent frontier
+    * additional plots that seem to help
+    * Add in more trade signals to relevant indicators to assist with decision making and interpretation
+    * Method to pass along custom settings to each plot routine
+    * Method to use each plot routine solo with no multiplot call
